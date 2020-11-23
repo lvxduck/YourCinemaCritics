4792008ca818ce7877e41748d534044a4ba50d58
lokishengoku <dthuanle.20@gmail.com>
2020-11-23 21:27:07 +0700

    create bottom navigation

---
+++ app/src/main/java/com/lduwcs/yourcinemacritics/fragments/ProfileFragment.java
@@ -0,0 +1,28 @@
+package com.lduwcs.yourcinemacritics.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.lduwcs.yourcinemacritics.R;
+
+public class ProfileFragment extends Fragment {
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
