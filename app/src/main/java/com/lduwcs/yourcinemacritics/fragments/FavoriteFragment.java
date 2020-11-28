9de41f9a6325fbbb25c2f88acdfae51d1c4acc3f
lokishengoku <dthuanle.20@gmail.com>
2020-11-28 10:07:23 +0700

    prepare v2

--- app/src/main/java/com/lduwcs/yourcinemacritics/fragments/FavoriteFragment.java
+++ app/src/main/java/com/lduwcs/yourcinemacritics/fragments/FavoriteFragment.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.lduwcs.yourcinemacritics.R;
 
 public class FavoriteFragment extends Fragment {
+    private CardView btnFilter, btnSort;
+    private SearchView edtSearch;
+    private RecyclerView favRecView;
+
     public FavoriteFragment() {
         // Required empty public constructor
     }
@@ -24,5 +33,15 @@ public class FavoriteFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorite, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnFilter = view.findViewById(R.id.btnFilter);
+        btnSort = view.findViewById(R.id.btnSort);
+        edtSearch = view.findViewById(R.id.edtSearch);
+        favRecView = view.findViewById(R.id.favoriteRecView);
     }
 }
--- app/src/main/java/com/lduwcs/yourcinemacritics/fragments/SearchFragment.java
+++ app/src/main/java/com/lduwcs/yourcinemacritics/fragments/SearchFragment.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.lduwcs.yourcinemacritics.R;
 
 public class SearchFragment extends Fragment {
+    private CardView btnFilter, btnSort;
+    private SearchView edtSearch;
+    private RecyclerView favRecView;
+
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -24,5 +33,15 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnFilter = view.findViewById(R.id.btnFilter);
+        btnSort = view.findViewById(R.id.btnSort);
+        edtSearch = view.findViewById(R.id.edtSearch);
+        favRecView = view.findViewById(R.id.favoriteRecView);
     }
 }
