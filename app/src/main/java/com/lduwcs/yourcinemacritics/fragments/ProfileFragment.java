8aee41a901b2a0d6ad46bb319d157fc84c22a7d4
lokishengoku <dthuanle.20@gmail.com>
2020-12-14 19:26:37 +0700

    modify darkmode button, add about button

--- app/src/main/java/com/lduwcs/yourcinemacritics/fragments/ProfileFragment.java
+++ app/src/main/java/com/lduwcs/yourcinemacritics/fragments/ProfileFragment.java
@@ -6,27 +6,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.lduwcs.yourcinemacritics.R;
+import com.lduwcs.yourcinemacritics.activities.AboutActivity;
 import com.lduwcs.yourcinemacritics.activities.LoginActivity;
 import com.lduwcs.yourcinemacritics.activities.MainActivity;
 import com.lduwcs.yourcinemacritics.uiComponents.NeuButton;
 
 
 public class ProfileFragment extends Fragment {
-    SwitchMaterial swDarkMode;
     private NeuButton btnLogout;
-    private TextView txtProfileEmail;
+    private NeuButton btnEditProfile;
+    private TextView txtProfileEmail, txtDarkModeContent;
+    private ConstraintLayout btnDarkMode, btnAbout;
+    private ImageView iconDarkMode;
     FirebaseUser user;
 
     public ProfileFragment() {
@@ -50,7 +54,13 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         btnLogout = view.findViewById(R.id.btnProfileLogout);
+        btnEditProfile = view.findViewById(R.id.btnEditProfile);
+        btnDarkMode = view.findViewById(R.id.btnDarkMode);
+        btnAbout = view.findViewById(R.id.btnAbout);
         txtProfileEmail = view.findViewById(R.id.txtProfileEmail);
+        txtDarkModeContent = view.findViewById(R.id.txtDarkModeContent);
+        iconDarkMode = view.findViewById(R.id.iconDarkMode);
+
         user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             String email = user.getEmail();
@@ -75,19 +85,60 @@ public class ProfileFragment extends Fragment {
             builder.show();
         });
 
-
-        swDarkMode = view.findViewById(R.id.btnDarkMode);
-        if (MainActivity.isDarkMode)
-            swDarkMode.setChecked(true);
-        swDarkMode.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (MainActivity.isDarkMode) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                MainActivity.editor.putBoolean("isDarkMode", false);
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                MainActivity.editor.putBoolean("isDarkMode", true);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAbout();
             }
-            MainActivity.editor.apply();
         });
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEdit();
+            }
+        });
+
+
+        if (MainActivity.isDarkMode) {
+            iconDarkMode.setImageResource(R.drawable.ic_round_wb_sunny_24);
+            txtDarkModeContent.setText(R.string.turn_on_light_mode);
+        } else {
+            iconDarkMode.setImageResource(R.drawable.ic_round_nights_stay_24);
+            txtDarkModeContent.setText(R.string.turn_on_dark_mode);
+        }
+
+
+        btnDarkMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (MainActivity.isDarkMode) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    MainActivity.editor.putBoolean("isDarkMode", false);
+                    MainActivity.editor.apply();
+                    iconDarkMode.setImageResource(R.drawable.ic_round_nights_stay_24);
+                    txtDarkModeContent.setText(R.string.turn_on_dark_mode);
+
+                    MainActivity.isDarkMode = false;
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    MainActivity.editor.putBoolean("isDarkMode", true);
+                    MainActivity.editor.apply();
+                    iconDarkMode.setImageResource(R.drawable.ic_round_wb_sunny_24);
+                    txtDarkModeContent.setText(R.string.turn_on_light_mode);
+
+                    MainActivity.isDarkMode = true;
+                }
+            }
+        });
+    }
+
+    private void onAbout() {
+        Intent intent = new Intent(getContext(), AboutActivity.class);
+        startActivity(intent);
+    }
+
+    private void onEdit() {
+        //do something
     }
 }
